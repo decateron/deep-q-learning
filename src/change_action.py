9765bb381cdec72474158f516002d5876d62391a
Oleh Korsunskyi <korsunskyi.job@gmail.com>
2019-09-03 09:49:11 +0200

    Closed #4 - add function that creates environment
    Add the function that creates an environment and returns the parameters of the environment.

---
+++ src/change_action.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+
+
+def action_to_command(action, action_size, commands):
+    """
+    Change from an action to command, because the environment takes only commands.
+    :param action: The action that produces our DQN
+    :return: The command
+    """
+
+    # Change from an array to a list
+    action = list(action)  
+
+    # Get the index of 1
+    index = action.index(1)
+
+    # Choose the comand
+    if index < action_size and index == 0:
+        command = commands[index]
+    elif index < action_size and index == 1:
+        command = commands[index]
+    elif index < action_size and index == 2:
+        command = commands[index]
+    elif index < action_size and index == 3:
+        command = commands[index]
+    elif index < action_size and index == 4:
+        command = commands[index]
+    elif index < action_size and index == 5:
+        command = commands[index]
+    elif index < action_size and index == 6:
+        command = commands[index]
+    elif index < action_size and index == 7:
+        command = commands[index]
+    elif index < action_size and index == 8:
+        command = commands[index]
+    elif index < action_size and index == 9:
+        command = commands[index]
+    elif index < action_size and index == 10:
+        command = commands[index]
+    elif index < action_size and index == 11:
+        command = commands[index]
+    elif index < action_size and index == 12:
+        command = commands[index]
+    elif index < action_size and index == 13:
+        command = commands[index]
+    elif index < action_size and index == 14:
+        command = commands[index]
+    elif index < action_size and index == 15:
+        command = commands[index]
+    elif index < action_size and index == 16:
+        command = commands[index]
+    elif index < action_size and index == 17:
+        command = commands[index]
+    elif index < action_size and index == 18:
+        command = commands[index]
+    elif index < action_size and index == 19:
+        command = commands[index]
+
---
+++ src/environment.py
@@ -0,0 +1,42 @@
+import retro
+import numpy as np
+
+
+
+game_commands = {'SpaceInvaders-Atari2600': np.array([[1, 0, 0, 0, 0, 0, 0, 0],
+                                                      [0, 0, 0, 0, 0, 0, 1, 0],
+                                                      [0, 0, 0, 0, 0, 0, 0, 1]])}
+
+
+
+def create_environment(lib_name, game, action_space, use_commands=False, game_commands=None):
+    """
+    Create environment. Return much useful information about the environment.
+
+    :param lib_name: The name of the library that contains our environment.
+    :param game: The name of the game we want to play.
+    :param action_space: How many actions we can take in this game ?
+    :param use_commands: Do we want to use commands ?
+    :param game_commands: Available commands in this game.
+    :return: environment object, action space, possible actions, commands.
+
+    """
+
+    # Create an environment
+    env = lib_name.make(game)
+    print("The size of our frame is: ", env.observation_space)
+
+    # Initialize possible actions
+    possible_actions = np.array(np.eye(action_space, dtype=int).tolist())
+
+    print()
+    print(possible_actions)
+
+    # Initialize commands 
+    if use_commands:
+        commands = game_commands[game]
+    else:
+        commands = False
+
+
+    return env, action_space, possible_actions, commands
